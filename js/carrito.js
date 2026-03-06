f0ef341afc30661678cd5d8fbc6db8e8d7508339
oscar-1202 <76372616@certus.edu.pe>
2026-03-06 16:00:49 -0500

    prueba

--- js/carrito.js
+++ js/carrito.js
@@ -2,13 +2,25 @@ function agregarAlCarrito(nombre, precio, imagen) {
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
     carrito.push({
+        id: Date.now(),
         nombre: nombre,
         precio: precio,
         imagen: imagen
     });
 
     localStorage.setItem("carrito", JSON.stringify(carrito));
-
     alert("🛒 Producto agregado al carrito");
 }
 
+function eliminarDelCarrito(id) {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+    // Filtramos para dejar fuera el producto con el id que recibimos
+    carrito = carrito.filter(producto => producto.id !== id);
+
+    // Guardamos el nuevo carrito actualizado
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    // Recargamos la lista para que el usuario vea que ya no está
+    location.reload(); 
+}
