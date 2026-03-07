237083e0bb78a52c76a3b5b76f3922a1f1f6d2ab
OD-Enilson <73120631@certus.edu.pe>
2026-03-06 19:38:49 -0500

    reubique el js de contactos

--- js/val_form.js
+++ js/val_form.js
@@ -1,18 +1,15 @@
 /*confirmancion de envio correcto de correo*/
 const correo = document.getElementById("correo");
 
-correo.addEventListener("input", function(){/*con addel se revisa constantemente las entradas y cada cambio dispara la funcion*/
+correo.addEventListener("input", function(){
 
-    if(correo.validity.valid){/*si cumple con con formato de un correo con @ */
-        /* correo.val*/
-        correo.classList.remove("error");
+    if(correo.validity.valid){
+        /*agrega y quita la clase*/
+        correo.classList.remove("error");  
         correo.classList.add("correcto");
-
     }else{
-
         correo.classList.remove("correcto");
         correo.classList.add("error");
-
     }
 
 });
@@ -22,9 +19,7 @@ const form = document.getElementById("form-contacto");
 const mensaje = document.getElementById("mensaje-envio");
 
 form.addEventListener("submit", function(e){
-
 e.preventDefault();
-
 mensaje.innerText = "Mensaje enviado correctamente ✔";
 
 });
@@ -33,15 +28,10 @@ mensaje.innerText = "Mensaje enviado correctamente ✔";
 const botonFloat = document.getElementById("whatsapp-float");
 
 window.addEventListener("scroll", function(){
-
 if(window.scrollY > 300){
-
-botonFloat.style.display = "block";
-
+    botonFloat.style.display = "block";
 }else{
-
-botonFloat.style.display = "none";
-
+    botonFloat.style.display = "none";
 }
 
 });
