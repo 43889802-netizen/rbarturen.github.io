8ee0cd2c17b4f0c7b812d69e7abb5e8dcc3db8ed
OD-Enilson <73120631@certus.edu.pe>
2026-03-06 19:00:25 -0500

    agregue dinamicas js en CONTACTOS

---
+++ val_form.js
@@ -0,0 +1,47 @@
+/*confirmancion de envio correcto de correo*/
+const correo = document.getElementById("correo");
+
+correo.addEventListener("input", function(){
+
+    if(correo.validity.valid){
+
+        correo.classList.remove("error");
+        correo.classList.add("correcto");
+
+    }else{
+
+        correo.classList.remove("correcto");
+        correo.classList.add("error");
+
+    }
+
+});
+
+/*confirmancion de enviuo correctro de datos*/
+const form = document.getElementById("form-contacto");
+const mensaje = document.getElementById("mensaje-envio");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+mensaje.innerText = "Mensaje enviado correctamente ✔";
+
+});
+
+/*animacion al boton de wsp*/
+const botonFloat = document.getElementById("whatsapp-float");
+
+window.addEventListener("scroll", function(){
+
+if(window.scrollY > 300){
+
+botonFloat.style.display = "block";
+
+}else{
+
+botonFloat.style.display = "none";
+
+}
+
+});
